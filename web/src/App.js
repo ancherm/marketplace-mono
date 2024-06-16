737a0a7eb0d01038f8d7c36cef65855c90604e0a
Ancherm <andrewcherm2014@gmail.com>
2024-06-17 01:23:06 +0400

    WEB: add Navbar(basic), edit ProductCard

--- web/src/App.js
+++ web/src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import {useState} from "react";
 import ProductCard from "./components/product-card/ProductCard";
 import ProductCardList from "./components/product-card/ProductCardList";
+import Navbar from "./components/ui/navbar/Navbar";
 
 function App() {
     const [productCard, setProductCart] = useState([
-        {id: 1, photo: "photo1", title: "title1", price: "price1"},
+        {id: 1, photo: "pics/Tragic_City.jpg", title: "ЛСП", price: "1500"},
         {id: 2, photo: "photo2", title: "title2", price: "price2"},
         {id: 3, photo: "photo3", title: "title3", price: "price3"},
         {id: 3, photo: "photo3", title: "title3", price: "price3"},
@@ -20,6 +21,7 @@ function App() {
 
     return (
         <div className="App">
+            <Navbar/>
             <ProductCardList productCard={productCard}/>
         </div>
     );
--- web/src/components/product-card/ProductCard.jsx
+++ web/src/components/product-card/ProductCard.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import classes from "./ProductCard.module.css";
+import Button from "@mui/material/Button";
 
 const ProductCard = (props) => {
 
     return (
         <div className={classes.productCard}>
-            <div className="productCard__photo">
-                {props.card.photo}
+            <div className={classes.productCard__photo}>
+                <img src={props.card.photo} alt={props.card.title} />
             </div>
-            <div className="productCard__title">
+            <div className={classes.productCard__title}>
                 {props.card.title}
             </div>
-            <div className="productCard__price">
-                {props.card.price}
+            <div className={classes.productCard__price}>
+                {props.card.price} ₽
             </div>
-            <div className="productCard__btn">
-                <button>В корзину</button>
-            </div>
+            <Button
+                className={classes.productCard__btn}
+                variant="contained"
+                style={{
+                    borderRadius: "10px",
+                    fontSize: "14px",
+                    cursor: "pointer",
+                    backgroundColor: "limegreen",
+                    color: "black"
+                }}
+            >
+                В корзину
+            </Button>
+
         </div>
     );
 };
